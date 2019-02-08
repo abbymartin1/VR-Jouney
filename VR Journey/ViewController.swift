8a0b1f6de50b7cb704bef06875578f47756942b4
Abby Martin <abby.c.martin@hotmail.com>
2019-02-08 16:39:32 -0500

    Initial Commit

---
+++ VR Journey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VR Journey
+//
+//  Created by Abby Martin on 2019-02-08.
+//  Copyright © 2019 Abby Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
